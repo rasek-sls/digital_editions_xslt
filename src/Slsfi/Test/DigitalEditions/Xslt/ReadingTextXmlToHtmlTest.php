7ccc12a3c7b38b08b4c26332dadaeafa08839c09
toffebjorkskog <christoffer.bjorkskog@genero.fi>
2016-11-03 16:30:53 +0200

    enabled unit testing

---
+++ src/Slsfi/Test/DigitalEditions/Xslt/ReadingTextXmlToHtmlTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Slsfi\Test\DigitalEditions\Xslt;
+
+class ReadingTextXmlToHtmlTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+}
+
