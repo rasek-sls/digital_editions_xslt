1cadd24effe288b8cfb62985de0e384120043dfa
toffebjorkskog <christoffer.bjorkskog@genero.fi>
2016-12-29 10:29:23 +0200

    added commentaries xst transformer

---
+++ src/Slsfi/DigitalEditions/Xslt/CommentariesTransformer.php
@@ -0,0 +1,11 @@
+<?php
+namespace Slsfi\DigitalEditions\Xslt;
+
+class CommentariesTransformer extends XmlToHtml {
+
+    public function __construct($xmlFilePath) {
+        parent::__construct(__DIR__ . "/../../../../xslt/com.xsl", $xmlFilePath);
+    }
+
+}
+
---
+++ src/Slsfi/Test/DigitalEditions/Xslt/CommentariesTransformerTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace Slsfi\Test\DigitalEditions\Xslt;
+
+use Slsfi\DigitalEditions\Xslt\CommentariesTransformer;
+
+class CommentariesTransformerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+        $this->assertTrue($foo);
+    }
+
+    public function testConstruction_validFilePath_shouldCreteObject() {
+        $xmlPath = __DIR__ . "/../../../Test/DigitalEditions/Xslt/xml-files/com/15_677_com.xml";
+        $object = new CommentariesTransformer($xmlPath);
+
+        $this->assertInstanceOf(CommentariesTransformer::class, $object);
+    }
+
+    /**
+     * @expectedException        Exception
+     * @expectedExceptionMessage XML File not found
+     */
+    public function testConstruction_inValidFilePath_shouldCreteObject() {
+        $xmlPath = "";
+        $object = new CommentariesTransformer($xmlPath);
+    }
+
+    /**
+     * @expectedException        Exception
+     * @expectedExceptionMessage XML File not found
+     */
+    public function testConstruction_nullFilePath_shouldCreteObject() {
+        $object = new CommentariesTransformer(null);
+    }
+
+
+    public function testGetHTML_validFilePath_shouldReturnHTML() {
+
+        $xmlPath = __DIR__ . "/../../../Test/DigitalEditions/Xslt/xml-files/com/15_677_com.xml";
+        $object = new CommentariesTransformer($xmlPath);
+        $html = $object->getHTML();
+
+        $this->assertContains('<h3 class=', $html);
+    }
+
+    public function testGetHTML_withOldNamespace_shouldUseNewNamespace() {
+        $xmlPath = __DIR__ . "/../../../Test/DigitalEditions/Xslt/xml-files/com/15_677_com.xml";
+        $object = new CommentariesTransformer($xmlPath);
+        $html = $object->getHTML();
+        $this->assertContains('<h3 class=', $html);
+    }
+
+}
+
